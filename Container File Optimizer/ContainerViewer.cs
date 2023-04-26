2e1aeed4bc00f569620925718f8dc6f58d880743
cjosh24 <123517602+cjosh24@users.noreply.github.com>
2023-04-25 20:10:39 -0400

    Added addFile and addFileConection functions to the container viewer. Also began refactor of container viewers code.

--- Container File Optimizer/ContainerViewer.cs
+++ Container File Optimizer/ContainerViewer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
@@ -8,8 +9,10 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
 
 namespace Container_File_Optimizer
 {
@@ -32,69 +35,98 @@ namespace Container_File_Optimizer
         }
 
 
+        /// <summary>
+        /// This function populates the container viewer with all of the app names and stores their ids in a Dictionary 
+        /// </summary>
         public void ViewContainers()
         {
             // Define SQL query to retrieve system_name and system_id columns
             string sqlQuery = "SELECT app_name, app_id FROM Application";
 
+
             // Create SqlConnection and SqlCommand objects
             using (SqlConnection cnn = new SqlConnection(connectionString))
             using (SqlCommand cmd = new SqlCommand(sqlQuery, cnn))
             {
-
-                // Create a new DataTable to hold the results
-                DataTable dataTable = new DataTable();
-
-                // Create a new SqlDataAdapter and fill the DataTable
-                using (SqlDataAdapter adp = new SqlDataAdapter(cmd))
+                try
                 {
-                    adp.Fill(dataTable);
+                    // Create a new DataTable to hold the results
+                    DataTable dataTable = new DataTable();
+
+                    // Create a new SqlDataAdapter and fill the DataTable
+                    using (SqlDataAdapter adp = new SqlDataAdapter(cmd))
+                    {
+                        adp.Fill(dataTable);
+                    }
+
+                    // Create a new Dictionary to store ListView index and system_id
+
+                    // Add each system_name value to the ListView and add ListView index and system_id to the dictionary
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+                        DataRow row = dataTable.Rows[i];
+                        string applicationName = row["app_name"].ToString();
+                        int applicationID = Convert.ToInt32(row["app_id"]);
+                        listBoxContainerViewer.Items.Add(applicationName);
+                        appIDCollection.Add(i, applicationID);
+                    }
+
+
                 }
-
-                // Create a new Dictionary to store ListView index and system_id
-
-                // Add each system_name value to the ListView and add ListView index and system_id to the dictionary
-                for (int i = 0; i < dataTable.Rows.Count; i++)
+                catch (SqlException ex)
                 {
-                    DataRow row = dataTable.Rows[i];
-                    string applicationName = row["app_name"].ToString();
-                    int applicationID = Convert.ToInt32(row["app_id"]);
-                    listBoxContainerViewer.Items.Add(applicationName);
-                    appIDCollection.Add(i, applicationID);
+
+                    //error message to show if error ocurs during delete
+                    MessageBox.Show("An error ocured!");
                 }
+               
             }
         }
 
 
 
-        // Gets the file names from the application ID. This is used to populate the files listbox
+        /// <summary>
+        /// Gets the file names from the application ID. This is used to populate the files listbox
+        /// </summary>
+        /// <param name="appID">The application id that the fiels are connected to.</param>
         public void GetFileNames(int appID)
         {
             string query = "SELECT * FROM AppFile af LEFT JOIN [File] fi ON af.file_id = fi.file_id WHERE af.app_id = @app_id";
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            using (SqlCommand command = new SqlCommand(query, connection))
+            try
             {
-                connection.Open();
-                command.Parameters.AddWithValue("@app_id", appID);
-
-                SqlDataReader reader = command.ExecuteReader();
-
-                // Iterate through the SqlDataReader and populate the CheckedListBox
-                while (reader.Read())
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlCommand command = new SqlCommand(query, connection))
                 {
-                    int app_id = reader.GetInt32(0); // see if this can work with Int16 
-                    int file_id = reader.GetInt32(1); // Same with this 
-                    string file_name = reader.GetString(3);
-                    listBoxFiles.Items.Add(file_name);
+                    connection.Open();
+                    command.Parameters.AddWithValue("@app_id", appID);
 
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    // Iterate through the SqlDataReader and populate the CheckedListBox
+                    while (reader.Read())
+                    {
+                        int app_id = reader.GetInt32(0); // see if this can work with Int16 
+                        int file_id = reader.GetInt32(1); // Same with this 
+                        string file_name = reader.GetString(3);
+                        listBoxFiles.Items.Add(file_name);
+
+                    }
+
+                    // Close the SqlDataReader and the SqlConnection
+                    reader.Close();
+
+                    connection.Close();
                 }
 
-                // Close the SqlDataReader and the SqlConnection
-                reader.Close();
-
-                connection.Close();
             }
+            catch (SqlException ex)
+            {
+
+                //error message to show if error ocurs during delete
+                MessageBox.Show("An error ocured!");
+            }
+            
         }
 
         /// <summary>
@@ -130,7 +162,7 @@ namespace Container_File_Optimizer
             {
 
                 //error message to show if error ocurs during delete
-                MessageBox.Show("An error ocured when attempting to delete the Application!" + ex);
+                MessageBox.Show("An error ocured when attempting to delete the Application!");
             }
 
             try
@@ -158,7 +190,7 @@ namespace Container_File_Optimizer
             {
 
                 //error message to show if error ocurs during delete
-                MessageBox.Show("An error ocured when attempting to delete the Application!" + ex);
+                MessageBox.Show("An error ocured when attempting to delete the Application!");
             }
 
 
@@ -187,7 +219,7 @@ namespace Container_File_Optimizer
             {
 
                 //error message to show if error ocurs during delete
-                MessageBox.Show("An error ocured when attempting to delete the Application!" + ex);
+                MessageBox.Show("An error ocured when attempting to delete the Application!");
             }
         }
 
@@ -227,7 +259,7 @@ namespace Container_File_Optimizer
             {
 
                 //error message to show if error ocurs during delete
-                MessageBox.Show("An error ocured when attempting to delete the file!" + ex);
+                MessageBox.Show("An error ocured when attempting to delete the file!");
             }
 
             if (fileCount <= 1)
@@ -297,7 +329,7 @@ namespace Container_File_Optimizer
             {
 
                 //error message to show if error ocurs during delete
-                MessageBox.Show("An error ocured!" + ex);
+                MessageBox.Show("An error ocured!");
             }
 
             return fileCount;
@@ -305,8 +337,10 @@ namespace Container_File_Optimizer
 
 
 
-
-        // Gets all of the apps that a file is contained in and returns their ids.
+        /// <summary>
+        /// Gets all of the file that a application is conected to and returns their ids.
+        /// </summary>
+        /// <param name="appID">The the id of the aplication the files are connect to.</param>
         public void GetFileIDS(int appID)
         {
             // Define SQL query to retrieve system_name and system_id columns
@@ -318,28 +352,157 @@ namespace Container_File_Optimizer
             using (SqlConnection cnn = new SqlConnection(connectionString))
             using (SqlCommand cmd = new SqlCommand(query, cnn))
             {
-                cmd.Parameters.AddWithValue("@app_id", appID);
-                // Create a new DataTable to hold the results
-                DataTable dataTable = new DataTable();
-
-                // Create a new SqlDataAdapter and fill the DataTable
-                using (SqlDataAdapter adp = new SqlDataAdapter(cmd))
+                try
                 {
-                    adp.Fill(dataTable);
+                    cmd.Parameters.AddWithValue("@app_id", appID);
+                    // Create a new DataTable to hold the results
+                    DataTable dataTable = new DataTable();
+
+                    // Create a new SqlDataAdapter and fill the DataTable
+                    using (SqlDataAdapter adp = new SqlDataAdapter(cmd))
+                    {
+                        adp.Fill(dataTable);
+                    }
+
+                    // Create a new Dictionary to store ListView index and system_id
+
+                    // Add each system_name value to the ListView and add ListView index and system_id to the dictionary
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+                        DataRow row = dataTable.Rows[i];
+                        int file_id = Convert.ToInt32(row["file_id"]);
+                        fileIDCollection.Add(i, file_id);
+                    }
+
                 }
-
-                // Create a new Dictionary to store ListView index and system_id
-
-                // Add each system_name value to the ListView and add ListView index and system_id to the dictionary
-                for (int i = 0; i < dataTable.Rows.Count; i++)
+                catch (SqlException ex)
                 {
-                    DataRow row = dataTable.Rows[i];
-                    int file_id = Convert.ToInt32(row["file_id"]);
-                    fileIDCollection.Add(i, file_id);
+
+                    //error message to show if error ocurs during delete
+                    MessageBox.Show("An error ocured!");
                 }
+                
             }
 
 
+        }
+
+        /// <summary>
+        /// This function creates a new file record in the [File] table of the database.
+        /// </summary>
+        /// <param name="filePath">The file path of the file to be added to the database.</param>
+        private void CreateFile(string filePath)
+        {
+            // Extract the file name and type from the file path.
+            string fileName = Path.GetFileName(filePath);
+            string pattern = @"(?<=\.)\w+$"; // matches the file extension after the dot
+            Match match = Regex.Match(fileName, pattern);
+            string fileType = match.Value;
+
+            // Connect to the database.
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Define the SQL query for inserting a new file record.
+                    string query = "INSERT INTO [File] (file_name, file_path, file_type) VALUES (@file_name, @file_path, @file_type)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@file_name", fileName);
+
+                    // Determine the file type based on its content.
+                    if (fileType == string.Empty) //If no extension file is a bin
+                    {
+                        command.Parameters.AddWithValue("@file_path", filePath);
+                        command.Parameters.AddWithValue("@file_type", ".bin");
+                        int rowsAffected = command.ExecuteNonQuery();
+                    }
+                    else if (fileType == "so") //if extension is so than file is a library
+                    {
+                        command.Parameters.AddWithValue("@file_path", filePath);
+                        command.Parameters.AddWithValue("@file_type", "." + fileType);
+                        int rowsAffected = command.ExecuteNonQuery();
+                    }
+                    else //any other extension is a config
+                    {
+                        command.Parameters.AddWithValue("@file_path", filePath);
+                        command.Parameters.AddWithValue("@file_type", ".config");
+                        int rowsAffected = command.ExecuteNonQuery();
+                    }
+
+                    // Close the database connection.
+                    connection.Close();
+
+
+                }
+                catch (SqlException ex)
+                {
+
+                    //error message to show if error ocurs during delete
+                    MessageBox.Show("An error ocured when adding the file!");
+                }
+               
+            }
+        }
+
+        /// <summary>
+        /// This method adds a connection between an application and a file in the database, based on the app ID and file path.
+        /// </summary>
+        /// <param name="appID">The application to connect the file to.</param>
+        /// /// <param name="filePath">The file path of the file to be added to the database.</param>
+        private void AddAppFileConnection(int appID, string filePath)
+        {
+            // Extract the file name from the file path using a regular expression.
+            string fileName = Regex.Match(filePath, @"(?<=\\)[^\\]*$").Value;
+
+            // Create a SQL connection using the connection string.
+            using (SqlConnection cnn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    cnn.Open();
+
+                    // Declare a variable to store the file ID.
+                    int fileID;
+
+                    // Define the SQL query to retrieve the file ID based on the file name and path.
+                    string query = "SELECT file_id FROM [File] WHERE file_name = @file_name AND file_path = @file_path";
+
+                    // Create a SQL command using the query and connection.
+                    SqlCommand command = new SqlCommand(query, cnn);
+
+                    // Add parameters to the command to specify the file name and path.
+                    command.Parameters.AddWithValue("@file_name", fileName);
+                    command.Parameters.AddWithValue("@file_path", filePath);
+
+                    // Execute the command and retrieve the file ID as a scalar value.
+                    fileID = (int)command.ExecuteScalar();
+
+                    // Define the SQL query to insert a new row into the AppFile table with the app ID and file ID.
+                    query = "INSERT INTO AppFile (app_id, file_id) VALUES (@app_id, @file_id)";
+
+                    // Create a new SQL command using the query and connection.
+                    command = new SqlCommand(query, cnn);
+
+                    // Add parameters to the command to specify the app ID and file ID.
+                    command.Parameters.AddWithValue("@app_id", appID);
+                    command.Parameters.AddWithValue("@file_id", fileID);
+
+                    // Execute the command to insert the new row.
+                    command.ExecuteNonQuery();
+
+                    // Close the SQL connection.
+                    cnn.Close();
+                }
+                catch (SqlException ex)
+                {
+
+                    //error message to show if error ocurs during delete
+                    MessageBox.Show("An error ocured when adding the file!");
+                }
+                
+            }
         }
 
         private void listBoxContainerViewer_SelectedIndexChanged(object sender, EventArgs e)
